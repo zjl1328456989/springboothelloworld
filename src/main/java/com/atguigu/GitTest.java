8893d113a4b02eb3e0ac90355bddbc37538ed413
zjl <1328456989@qq.com>
2020-07-05 12:59:45 +0800

    git联系

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("1111111BBBB");
     }
 }
